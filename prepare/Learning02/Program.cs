3751314075e676769d9b1fab6c22d76c06322e41
elannytorres <torres.elanny@gmail.com>
2023-11-02 21:37:44 -0500

    add: Abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
+
+// The C# convention is to start member variables with an underscore _
+
+// A special method, called a constructor that is invoked using the  
+// new keyword followed by the class name and parentheses.
+
+//Class: Person
+//Attributes:
+//* _givenName : string
+//* _familyName : string
+
+//Behaviors:
+//* ShowEasternName() : void
+//* ShowWesternName() : void
